5c9034b632930b56fd7521a9183323ebf7394d5f
Joseph Park <rkfnahs12e@gmail.com>
2024-05-16 21:10:51 +0900

    refactor : window에 이벤트핸들러를 걸지 않고 html 요소에 직접 걸게 수정

--- assignments/02-pikachu/src/components/Pikachu/Pikachu.jsx
+++ assignments/02-pikachu/src/components/Pikachu/Pikachu.jsx
@@ -1,50 +1,53 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./pikachu.css";
 
 function Pikachu() {
   const [locationX, setLocationX] = useState(0);
   const [locationY, setLocationY] = useState(0);
   const [isJumping, setIsJumping] = useState(false);
+  const grassRef = useRef(null);
 
+  const handlePikachuMove = (event) => {
+    switch (event.key) {
+      case "ArrowUp":
+        event.preventDefault();
+        setLocationY((prev) => Math.max(prev - 30, 0));
+        break;
+      case "ArrowDown":
+        event.preventDefault();
+        setLocationY((prev) => Math.min(prev + 30, 300));
+
+        break;
+      case "ArrowLeft":
+        event.preventDefault();
+        setLocationX((prev) => Math.max(prev - 30, 0));
+        break;
+      case "ArrowRight":
+        event.preventDefault();
+        setLocationX((prev) => Math.min(prev + 30, 300));
+        break;
+      case " " || "Spacebar":
+        event.preventDefault();
+        if (!isJumping && locationY) {
+          setIsJumping(true);
+          setTimeout(() => setIsJumping(false), 1000);
+        }
+        break;
+    }
+  };
   useEffect(() => {
-    const handlePikachuMove = (event) => {
-      switch (event.key) {
-        case "ArrowUp":
-          event.preventDefault();
-          setLocationY((prev) => Math.max(prev - 30, 0));
-          break;
-        case "ArrowDown":
-          event.preventDefault();
-          setLocationY((prev) => Math.min(prev + 30, 300));
-
-          break;
-        case "ArrowLeft":
-          event.preventDefault();
-          setLocationX((prev) => Math.max(prev - 30, 0));
-          break;
-        case "ArrowRight":
-          event.preventDefault();
-          setLocationX((prev) => Math.min(prev + 30, 300));
-          break;
-        case " " || "Spacebar":
-          event.preventDefault();
-          if (!isJumping && locationY) {
-            setIsJumping(true);
-            setTimeout(() => setIsJumping(false), 1000);
-          }
-          break;
-      }
-    };
-
-    window.addEventListener("keydown", handlePikachuMove);
-
-    return () => {
-      window.removeEventListener("keydown", handlePikachuMove);
-    };
-  });
-
+    grassRef.current.focus();
+  }, [grassRef]);
   return (
-    <div id="Grass">
+    <div
+      id="Grass"
+      ref={grassRef}
+      tabIndex={0} //tabIndex속성은 HTML요소에 포커스를 받을 수 있도록 설정한다.
+      // 대부분의 브라우저는 키보드 이벤트를 수신할 수 없기에 설정함으로서 해다 요소가 포커스를 받을 수 있게되고, 키보드 이벤트를 정상적으로 수신 가능함.
+      // 근데 뭔가 다른 방법이 있을 듯한 뭔가 이상함
+      onKeyDown={handlePikachuMove}
+      style={{ outline: "none" }}
+    >
       <div
         id="Pikachu"
         className={isJumping ? "jumping" : ""}
